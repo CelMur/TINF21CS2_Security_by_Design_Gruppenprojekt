97c26b4b65243e947a23caefcdfb90a780b8e597
kevin220901 <s211301@student.dhbw-mannheim.de>
2023-10-21 18:24:32 +0200

    Update Docker Environment

--- Project/app/electricity/settings.py
+++ Project/app/electricity/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'example.urls'
+ROOT_URLCONF = 'electricity.urls'
 
 TEMPLATES = [
     {
@@ -76,7 +76,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'example.wsgi.application'
+WSGI_APPLICATION = 'electricity.wsgi.application'
 
 
 # Database
