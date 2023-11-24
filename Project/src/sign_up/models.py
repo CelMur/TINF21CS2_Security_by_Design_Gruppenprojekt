05150d57943e2210cf4c86c628b9c5012a9f8292
Nevermindx3 <78419513+Nevermindx3@users.noreply.github.com>
2023-11-24 19:36:08 +0100

    worked on the registration_from

---
+++ Project/src/sign_up/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+
+
+class CustomUser(AbstractUser):
+    postal_code = models.CharField(max_length=10)
+    firstname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+
+    # Set unique related names for groups and user_permissions
+    groups = models.ManyToManyField(Group, related_name='customuser_groups')
+    user_permissions = models.ManyToManyField(Permission, related_name='customuser_user_permissions')
+
+    class Meta:
+        app_label = 'sign_up'
