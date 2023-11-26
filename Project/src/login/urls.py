0ad778eab09c37a561300cb8b625a6311e28421d
kevin220901 <s211301@student.dhbw-mannheim.de>
2023-11-26 18:46:20 +0100

    Implementierung Login Funktionalität

--- Project/src/login/urls.py
+++ Project/src/login/urls.py
@@ -3,5 +3,5 @@ from .views import LoginPageView
 app_name = "login"
 
 urlpatterns = [
-    path("", LoginPageView.as_view() , name="login"),
+    path("", LoginPageView , name="login"),
 ]
--- Project/src/login/views.py
+++ Project/src/login/views.py
@@ -1,6 +1,18 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
+def LoginPageView(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
 
-# Create your views here.
-class LoginPageView(TemplateView):
-    template_name =  "Login.html"
+        if user is not None:
+            login(request, user)
+            return redirect('/admin/')  
+        else:
+            messages.error(request, 'Benutzername oder Passwort ungültig.')
+
+    return render(request, 'Login.html')
