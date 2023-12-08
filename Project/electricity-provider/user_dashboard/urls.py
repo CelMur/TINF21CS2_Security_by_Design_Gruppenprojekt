9506b7b9c1cadccf669ea6aa2e259e3a68022ab2
kevin220901 <s211301@student.dhbw-mannheim.de>
2023-12-08 21:40:30 +0100

    Update

--- Project/electricity-provider/user_dashboard/urls.py
+++ Project/electricity-provider/user_dashboard/urls.py
@@ -1,7 +1,7 @@
 # project/urls.py
 from django.urls import path, include
 from .views import UserDashboardView
-app_name = "dashboard"
+app_name = "user_dashboard"
 
 urlpatterns = [
     path("dashboard/", UserDashboardView.as_view(), name = "user_dashboard"),
