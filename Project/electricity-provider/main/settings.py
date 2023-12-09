453f8d1233c05a5d6ffff5735304236645936163
Luis Eckert <s211288@student.dhbw-mannheim.de>
2023-12-10 00:44:28 +0100

    added django module registration

--- Project/electricity-provider/main/settings.py
+++ Project/electricity-provider/main/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
+    'registration'
 ]
 
 MIDDLEWARE = [
