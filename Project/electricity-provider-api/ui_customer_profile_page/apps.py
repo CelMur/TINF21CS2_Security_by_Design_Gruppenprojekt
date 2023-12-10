7e6430794f9f0f1d72465b99dd9297581d05c8bb
kevin220901 <s211301@student.dhbw-mannheim.de>
2023-12-10 21:46:39 +0100

    Update Apps

--- Project/electricity-provider-api/ui_customer_profile_page/apps.py
+++ Project/electricity-provider-api/ui_customer_profile_page/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class UiCustomerProfilePageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ui_costomer_profile_page'
+    name = 'ui_customer_profile_page'
--- Project/electricity-provider-api/ui_register_page/apps.py
+++ Project/electricity-provider-api/ui_register_page/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class UiRegistrationPageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ui_registration_page'
+    name = 'ui_register_page'
