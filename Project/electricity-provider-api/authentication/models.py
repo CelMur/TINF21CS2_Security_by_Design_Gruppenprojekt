335e0a9c4bb21153606f00b183694925fd941d6c
Luis Eckert <s211288@student.dhbw-mannheim.de>
2023-12-10 03:27:54 +0100

    fixed var assignment in authentication

--- Project/electricity-provider-api/authentication/models.py
+++ Project/electricity-provider-api/authentication/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
 class User(AbstractBaseUser):
-    id = id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = models.CharField(max_length=45, unique=True)
     last_name = models.CharField(max_length=45, unique=True)
     email = models.EmailField(max_length=45,unique=True,blank=False)
