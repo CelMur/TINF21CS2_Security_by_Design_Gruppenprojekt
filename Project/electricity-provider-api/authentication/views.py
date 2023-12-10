09c47e26d63ecd68180e1ae6cd0e80ebd00c5936
Luis Eckert <s211288@student.dhbw-mannheim.de>
2023-12-10 10:58:05 +0100

    added new db migrations; added on failed loginattempts increment counter

--- Project/electricity-provider-api/authentication/views.py
+++ Project/electricity-provider-api/authentication/views.py
@@ -1,12 +1,14 @@
+import hashlib
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model
+from django.db.models import F
 from django.contrib.auth import authenticate, login
 
 from utils.logger import *
 
-from .models import User
 from .serializers import UserAuthenticationSerializer
 
 class UserAuthenticationView(APIView):
@@ -26,5 +28,9 @@ class UserAuthenticationView(APIView):
             serializer = UserAuthenticationSerializer(instance=user)
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            logger.critical(f'failed authentication for user {username}')
+            if username is not None:
+                get_user_model().objects.filter(email=username).update(failed_login_attempts=F('failed_login_attempts') + 1)
+                #masking of the username to prevent information leakage in the logs
+                hashed_username = hashlib.sha256(username.encode('utf-8')).hexdigest()
+                logger.warn(f'failed authentication for user {hashed_username}')
             return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
