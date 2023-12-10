90bf16df6e8b328f85b2edad773f895ea99219b5
kevin220901 <s211301@student.dhbw-mannheim.de>
2023-12-10 19:29:42 +0100

    Update React

--- Project/electricity-provider-portal2/provider-portal/src/index.js
+++ Project/electricity-provider-portal2/provider-portal/src/index.js
@@ -11,64 +11,56 @@ window.$ = $;
 function Main() {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = "${process.env.PUBLIC_URL}/js/popper.min.js";
+    script.src = "./js/popper.min.js";
     script.async = true;
     document.body.appendChild(script);
   }, []);
 
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = "${process.env.PUBLIC_URL}/js/popper.min.js";
+    script.src = "./js/popper.min.js";
     script.async = true;
     document.body.appendChild(script);
   }, []);
   
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = "${process.env.PUBLIC_URL}/js/bootstrap.min.js";
+    script.src = "./js/bootstrap.min.js";
     script.async = true;
     document.body.appendChild(script);
   }, []);
   
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = "${process.env.PUBLIC_URL}/js/bootstrap-progressbar.min.js";
+    script.src = "./js/bootstrap-progressbar.min.js";
     script.async = true;
     document.body.appendChild(script);
   }, []);
   
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = "${process.env.PUBLIC_URL}/js/masonry.pkgd.min.js";
+    script.src = "./js/masonry.pkgd.min.js";
     script.async = true;
     document.body.appendChild(script);
   }, []);
   
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = "${process.env.PUBLIC_URL}/js/swiper.jquery.min.js";
+    script.src = "./js/swiper.jquery.min.js";
     script.async = true;
     document.body.appendChild(script);
   }, []);
   
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = "${process.env.PUBLIC_URL}/js/swiperRunner.js";
+    script.src = "./js/swiperRunner.js";
     script.async = true;
     document.body.appendChild(script);
   }, []);
   
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = "${process.env.PUBLIC_URL}/js/headroom.min.js";
-    script.async = true;
-    document.body.appendChild(script);
-  }, []);
-  
-  
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = "${process.env.PUBLIC_URL}/js/jquery.marquee.min.js";
+    script.src = "./js/headroom.min.js";
     script.async = true;
     document.body.appendChild(script);
   }, []);
@@ -76,21 +68,7 @@ function Main() {
   
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = "${process.env.PUBLIC_URL}/js/rangeslider.min.js";
-    script.async = true;
-    document.body.appendChild(script);
-  }, []);
-  
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = "${process.env.PUBLIC_URL}/js/waypoints.min.js";
-    script.async = true;
-    document.body.appendChild(script);
-  }, []);
-  
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = "${process.env.PUBLIC_URL}/js/jquery.counterup.min.js";
+    script.src = "./js/jquery.marquee.min.js";
     script.async = true;
     document.body.appendChild(script);
   }, []);
@@ -98,14 +76,36 @@ function Main() {
   
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = "${process.env.PUBLIC_URL}/js/gmap3.min.js";
+    script.src = "./js/rangeslider.min.js";
     script.async = true;
     document.body.appendChild(script);
   }, []);
   
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = "${process.env.PUBLIC_URL}/js/main.js";
+    script.src = "./js/waypoints.min.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+  
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "./js/jquery.counterup.min.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+  
+  
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "./js/gmap3.min.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+  
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "./js/main.js";
     script.async = true;
     document.body.appendChild(script);
   }, []);
