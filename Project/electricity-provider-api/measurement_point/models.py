93a71cad1f954185bbbba22017c84834fd2ae96e
Luis Eckert <s211288@student.dhbw-mannheim.de>
2023-12-10 13:12:55 +0100

    added household_size to measurement_point

--- Project/electricity-provider-api/measurement_point/models.py
+++ Project/electricity-provider-api/measurement_point/models.py
@@ -8,6 +8,9 @@ class MeasurementPoint(models.Model):
     is_active = models.BooleanField(default=True)
     latest_reading = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     latest_reading_date = models.DateField(null=True, blank=True)
+    household_size = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
     class Meta:
         db_table = "measurement_point"
