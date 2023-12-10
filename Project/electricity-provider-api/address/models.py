a9074c708ba52a699e2562b6fc421623ade36f17
Luis Eckert <s211288@student.dhbw-mannheim.de>
2023-12-10 13:55:44 +0100

    updated measurement_point to validate the given address

--- Project/electricity-provider-api/address/models.py
+++ Project/electricity-provider-api/address/models.py
@@ -1,8 +1,10 @@
+import uuid
 from django.conf import settings
 from django.db import models
 
 # Create your models here.
 class Address(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     street = models.CharField(max_length=255)
     street_number = models.CharField(max_length=255)
     postal_code = models.CharField(max_length=255)
--- Project/electricity-provider-api/measurement_point/serializers.py
+++ Project/electricity-provider-api/measurement_point/serializers.py
@@ -1,9 +1,69 @@
 
+import uuid
 from rest_framework import serializers
+from address.models import Address
+
 from .models import MeasurementPoint
 
 class MeasurementPointSerializer(serializers.ModelSerializer):
     class Meta:
         model = MeasurementPoint
-        fields = ('meter_uid', 'address', 'is_active', 'latest_reading', 'latest_reading_date', 'created_at')
+        fields = ('meter_uid', 'address', 'is_active', 'household_size','latest_reading', 'latest_reading_date', 'created_at')
         read_only_fields = ('meter_uid', 'created_at', 'latest_reading_date')
+
+
+    def validate_address(self, value):
+        '''
+        Validates the address field.
+        The address field can be either a reference to an existing address or a dictionary containing the address data.
+
+        Args:
+            value (int or dict): The value of the address field.
+
+        Returns:
+            Address: The validated address object.
+
+        Raises:
+            ValidationError: If the address field is invalid.
+        Raises:
+            Exception: If any other error occurs (=> probably a bug in the code, rofl >_<)
+
+        '''
+        address = None
+        if isinstance(value, serializers.UUIDField):
+            # Handle address reference
+            address = Address.objects.filter(id=value).first()
+            if address is None:
+                raise serializers.ValidationError("Invalid address reference.")
+        elif isinstance(value, dict):
+            # Handle address data
+            #TODO: this should be handled by the AddressSerializer
+            if 'street' not in value or 'city' not in value or 'street_number' not in value:
+                raise serializers.ValidationError("Invalid address data. Missing required fields.")
+            else:
+                address = Address.objects.create(**value)
+        else:
+            raise serializers.ValidationError("Invalid address format.")
+        return address
+
+    def validate_household_size(self, value):
+        '''
+        Checks if the household size is valid. 300 >= household_size > 0
+
+        Args:
+            value (int): The value of the household_size field.
+
+        Returns:
+            int: The validated household_size.
+
+        Raises:
+            ValidationError: If the household_size is invalid.
+        Raises:
+            Exception: If any other error occurs (=> probably a bug in the code, !..!)
+        '''
+        if value <= 0:
+            raise serializers.ValidationError("Household size must be a positive integer.")
+        if value > 300:
+            raise serializers.ValidationError("Household size cannot exceed 300.")
+        return value
+        
