47cd576d0c79139ad0cbc0e1de474b91caa389d2
Luis Eckert <s211288@student.dhbw-mannheim.de>
2023-12-10 08:45:28 +0100

    changed MeasurementPoint.is_active default = True

--- Project/electricity-provider-api/measurement_point/models.py
+++ Project/electricity-provider-api/measurement_point/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class MeasurementPoint(models.Model):
     meter_uid = models.CharField(max_length=255, unique=True, editable=False)
     address = models.ForeignKey('address.Address', on_delete=models.CASCADE)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     latest_reading = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     latest_reading_date = models.DateField(null=True, blank=True)
 
