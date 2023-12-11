547173d787c807492ece550ddc47efee24ac42d4
kevin220901 <s211301@student.dhbw-mannheim.de>
2023-12-11 02:56:55 +0100

    Restrict Paths

--- Project/electricity-provider-api/ui_customer_profile_page/views.py
+++ Project/electricity-provider-api/ui_customer_profile_page/views.py
@@ -1,8 +1,8 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
-# Create your views here.
-
-class UiCustomerProfilePage(TemplateView):
+class UiCustomerProfilePage(LoginRequiredMixin, TemplateView):
+    login_url = '/login/'  # Redirect URL if user is not authenticated
     template_name =  "ui_customer_profile_page.html"
 
     def get_context_data(self, **kwargs):
--- Project/electricity-provider-api/ui_logout_page/views.py
+++ Project/electricity-provider-api/ui_logout_page/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
 from django.views import View
 
-class LogoutView(View):
+class LogoutView(LoginRequiredMixin, View):
+    login_url = '/login/'  # Redirect URL if user is not authenticated
+
     def get(self, request):
         logout(request)
         return redirect('/')
