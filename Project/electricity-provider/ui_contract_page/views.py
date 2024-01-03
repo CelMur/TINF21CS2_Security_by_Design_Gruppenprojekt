3f18793613dc70d44cce58fc6b312227a2ba14bb
Luis Eckert <s211288@student.dhbw-mannheim.de>
2024-01-03 21:07:29 +0100

    added page new contract

--- Project/electricity-provider/ui_contract_page/views.py
+++ Project/electricity-provider/ui_contract_page/views.py
@@ -11,3 +11,15 @@ class UiContractPage(LoginRequiredMixin, TemplateView):
         # You can add any additional context data here
 
         return context
+    
+
+class UiNewContractPage(LoginRequiredMixin, TemplateView):
+    login_url = '/login/'  # Redirect URL if user is not authenticated
+    template_name =  "ui_new_contract_page.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # You can add any additional context data here
+
+        return context
