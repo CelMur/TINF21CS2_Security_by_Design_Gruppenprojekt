3faae228ba01bc3ea02d8ca3c3803befe6276a8a
CWinkler03 <cynthia@davidwinkler.de>
2024-01-03 21:08:41 +0100

    Vertradgsdashboard

--- Project/electricity-provider/init.sh
+++ Project/electricity-provider/init.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-
 sleep 15
 # Start the server in the background
 python3 manage.py runserver 0.0.0.0:8000 &
-
 # Perform the migration
 python3 manage.py migrate
-
 # Wait for the server process to finish
 wait %1
