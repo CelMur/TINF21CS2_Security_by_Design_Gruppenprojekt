14f836643319a9c8ad7df4d6d312890bec97b418
Luis Eckert <s211288@student.dhbw-mannheim.de>
2024-01-04 15:42:24 +0100

    updated init.sh

--- Project/electricity-provider/init.sh
+++ Project/electricity-provider/init.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+echo "Waiting for postgres..."
 sleep 15
+echo "Trying to run server..."
 # Start the server in the background
 python3 manage.py runserver 0.0.0.0:8000 &
 # Perform the migration
 python3 manage.py migrate
+# initialize the database with data
+python3 manage.py create_initial_energy_tariffs
 # Wait for the server process to finish
 wait %1
