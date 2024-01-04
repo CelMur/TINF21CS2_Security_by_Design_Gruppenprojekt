1c13e5cfa227f817d5807bcd22ec5fc85d3bab4f
Luis Eckert <s211288@student.dhbw-mannheim.de>
2024-01-04 18:57:09 +0100

    measurement_point: meter_uid now is primary key

--- Project/electricity-provider/measurement_point/models.py
+++ Project/electricity-provider/measurement_point/models.py
@@ -1,9 +1,9 @@
+import uuid
 from django.db import models
 
 # Create your models here.
 class MeasurementPoint(models.Model):
-    
-    meter_uid = models.CharField(max_length=255, unique=True, editable=False)
+    meter_uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     address = models.ForeignKey('address.Address', on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
     latest_reading = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
