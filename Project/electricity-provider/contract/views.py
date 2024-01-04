b5a4f788d1efb37759d144fa584fcf0a8dca9990
Luis Eckert <s211288@student.dhbw-mannheim.de>
2024-01-04 16:40:25 +0100

    added cancelation button for contracts (not tested)

--- Project/electricity-provider/contract/views.py
+++ Project/electricity-provider/contract/views.py
@@ -3,16 +3,16 @@ from datetime import datetime
 from django.conf import settings
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.response import Response
 
-from energy_tariff.models import EnergyTariff
-from address.models import Address
-from bank_account.models import BankAccount
 from .models import Contract
 
 from measurement_point.views import CreateMeasurementPointView
 from .serializers import ContractSerializerForCreate
 
 from utils.logger import *
+
 
 
 # Create your views here.
@@ -27,3 +27,9 @@ class ContractView(ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def destroy(self, request, *args, **kwargs):
+        contract = self.get_object()
+        contract.is_active = False
+        contract.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
